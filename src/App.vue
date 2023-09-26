5faee5c65b2172ee16666935941085df82ef020a
Alex <adruzenko03@gmail.com>
2023-09-25 23:32:59 -0400

    Fixed styling made into accordian

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <!-- eslint-disable vue/require-v-for-key -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <button type="button" class="btn btn-primary">Primary</button>
-  <div class="list-group" v-for="movie in topMovies">
-  <a href="#" class="list-group-item list-group-item-action">
-    {{movie.title}}
-  </a>
+
+  <div class="accordion" id="accordionExample">
+    <div  v-for="movie in topMovies" class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#a'+movie.film_id">
+        {{ movie.title }}
+      </button>
+    </h2>
+    <div :id="'a'+movie.film_id" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+
 
 </div></template>
 
@@ -26,5 +35,5 @@ export default {
 </script>
 
 <style>
-@import'~bootstrap/dist/css/bootstrap.css'
+@import 'bootstrap/dist/css/bootstrap.css'
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-
-import "bootstrap";
+import "bootstrap"
 
 
 createApp(App).mount('#app')
