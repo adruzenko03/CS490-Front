7eb0dcbc390a40892d89882d285a25fe9c3bf9ea
Alex <adruzenko03@gmail.com>
2023-10-12 10:42:41 -0400

    Customer Update

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -5,7 +5,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <button @click="reset" type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
@@ -59,6 +59,74 @@
   </div>
 
 
+  <div class="modal fade" id="upCust" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button @click="reset" type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <h1>Update Customer</h1>
+              <p v-if="this.errMess != ''" style="font-size: x-large;margin-bottom: 0;">{{ this.errMess }}</p>
+              <label>Customer Name:</label>
+                <input onkeydown="return /[a-z ]/i.test(event.key)" v-model="custName">
+                <br />
+              <form>
+                <label>Email:</label>
+                <input type="email" v-model="email">
+                <button @click="upCust('email')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>First Name:</label>
+                <input onkeydown="return /[a-z]/i.test(event.key)" v-model="firstName">
+                <button @click="upCust('firstName')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Last Name:</label>
+                <input onkeydown="return /[a-z]/i.test(event.key)" v-model="lastName">
+                <button @click="upCust('lastName')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Store:</label>
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                  <input v-model="store" type="radio" class="btn-check" :value="1" id="1" autocomplete="off" checked>
+                  <label class="btn btn-outline-primary" for="1">Store 1</label>
+
+                  <input v-model="store" type="radio" class="btn-check" :value="2" id="2" autocomplete="off">
+                  <label class="btn btn-outline-primary" for="2">Store 2</label>
+                </div>
+                <button @click="upCust('store')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Address 1:</label>
+                <input onkeydown="return /[a-z 0-9.-]/i.test(event.key)" v-model="address">
+                <button @click="upCust('address')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Address 2:</label>
+                <input onkeydown="return /[a-z 0-9.-]/i.test(event.key)" v-model="address2">
+                <button @click="upCust('address2')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>District:</label>
+                <input onkeydown="return /[a-z]/i.test(event.key)" v-model="district">
+                <button @click="upCust('district')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>City:</label>
+                <input onkeydown="return /[a-z]/i.test(event.key)" v-model="city">
+                <button @click="upCust('city')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Phone:</label>
+                <input inputmode="numeric" v-model="phone">
+                <button @click="upCust('phone')" type="button" class="btn btn-primary">Update</button>
+                <br />
+                <label>Postal Code:</label>
+                <input inputmode="numeric" v-model="postalCode">
+                <button @click="upCust('postalCode')" type="button" class="btn btn-primary">Update</button>
+                <br />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
   <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
     <input v-model="selected" type="radio" class="btn-check" :value="'ID'" id="ID" autocomplete="off" checked>
@@ -83,6 +151,9 @@
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newCust">
       New Customer
     </button>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#upCust">
+      Update Customer
+    </button>
   </div>
 
   <div class="accordion" id="accordionExample">
@@ -100,6 +171,8 @@
               <h6><strong>Email: </strong>{{ customer.email }}</h6>
               <h6><strong>Main Store: </strong>{{ customer.store_id }}</h6>
               <h6><strong>Active? </strong><span v-if="customer.active == 1">Yes</span><span v-else>No</span></h6>
+              <h6><strong>Address: </strong>{{ customer.address + ' ' + customer.address2 + ', ' + customer.district }}</h6>
+              <h6><strong>Phone: </strong>{{ customer.phone }}</h6>
               <h6><strong>Currently Rented Movies:</strong>
                 <div v-if="customer.rented">
                   <div class="row" v-for="movie in customer.rented.split(',')">
@@ -138,7 +211,7 @@ export default {
     return {
       selected: "ID", inp: "", confirm: 0, removedMovies: [],
       email: "", firstName: "", lastName: "", store: 1, address: "", address2: "", district: "", city: "", postalCode: "",
-      errMess: ""
+      errMess: "", custName: ""
     }
   },
   computed: {
@@ -158,6 +231,17 @@ export default {
     },
     reset() {
       this.confirm = 0
+      this.email = '',
+      this.firstName = '',
+      this.lastName = '',
+      this.address = '',
+      this.address2 = '',
+      this.district = '',
+      this.city = '',
+      this.phone = '',
+      this.postalCode = ''
+      this.custName=''
+      this.errMess=''
     },
     deleteCust(custId) {
       if (this.confirm == 0) {
@@ -185,20 +269,34 @@ export default {
       else if (!state.queries.cities.includes(this.city))
         this.errMess = "Error: Please include a valid city"
       else
-        socket.emit('addCust', { email: this.email, firstName: this.firstName, lastName: this.lastName, store:this.store,address: this.address, address2: this.address2, district: this.district, postalCode: this.postalCode, phone: this.phone, city: this.city })
-        socket.on('addConf',()=>{
-          this.email='',
-          this.firstName='',
-          this.lastName='',
-          this.address='',
-          this.address2='',
-          this.district='',
-          this.city='',
-          this.phone='',
-          this.postalCode=''
-          this.errMess="Success!"
-          this.submit()
-        })
+        socket.emit('addCust', { email: this.email, firstName: this.firstName, lastName: this.lastName, store: this.store, address: this.address, address2: this.address2, district: this.district, postalCode: this.postalCode, phone: this.phone, city: this.city })
+      socket.on('addConf', () => {
+        this.email = '',
+          this.firstName = '',
+          this.lastName = '',
+          this.address = '',
+          this.address2 = '',
+          this.district = '',
+          this.city = '',
+          this.phone = '',
+          this.postalCode = ''
+        this.errMess = "Success!"
+        this.submit()
+      })
+    },
+    upCust(upVar){
+      if(upVar=='phone' && this.phone.length!=10){
+        this.errMess='Incorrect Lenght for Phone'
+        return
+      }
+      if(this.custName==''){
+        this.errMess='Which customer are you updating?'
+        return
+      }
+      socket.emit('upCust',{"type":upVar,[upVar]:eval("this."+upVar),"customer":this.custName})
+      socket.on('upConf',(mess)=>{
+        this.errMess=mess
+      })
     }
   },
   beforeMount() {
